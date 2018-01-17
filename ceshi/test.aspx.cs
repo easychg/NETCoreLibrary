105e6789ad1902b631ae9fbfa50886eebeeb18c6
easychg <156375001@qq.com>
2018-01-17 14:23:48 +0800

    add

---
+++ ceshi/test.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using CoreLibrary;
+
+namespace Web
+{
+    public partial class test : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ExcelHelper excle = new ExcelHelper();
+            
+            Response.AddHeader("Content-Disposition", string.Format("attachment; filename={0}.xls", DateTime.Now.ToString("yyyyMMddHHmmssfff")));
+            Response.BinaryWrite(excle.Export());
+        }
+    }
+}
